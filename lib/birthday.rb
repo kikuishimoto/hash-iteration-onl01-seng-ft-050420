626cdcf768d32faac1e58879ae305de71564833e
kiku ishimoto <kikuish@gmail.com>
2020-05-07 19:42:30 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,8 +4,14 @@
 #   "Amanda" => 27
 # }
 
-def happy_birthday(birthday_kids)
+def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!" if age < 12
+  end# add your code snippet here!
+end
+
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end# add your code snippet here!
 end
